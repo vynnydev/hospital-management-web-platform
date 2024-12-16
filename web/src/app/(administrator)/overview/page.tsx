9e458499595afca70626b5862474b3d89c4ae641
vynnydev <vynnyprudencio@gmail.com>
2024-12-16 15:45:50 -0300

    feat: added main dashboard metrics structure

--- web/src/app/(administrator)/overview/page.tsx
+++ web/src/app/(administrator)/overview/page.tsx
@@ -1,15 +1,14 @@
 'use client'
 import React from 'react';
 
+import DashboardMetrics from './components/DashboardMetrics';
 import AgrixiAssistant from '@/components/ui/agrixi-assistant/IAgrixiAssistant';
 
 const Overview: React.FC = () => {
   
   return (
-    <div className="px-4 mt-[-80px]">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mb-8">
-        Overview
-      </div>
+    <div className="px-4 -mt-[100px]">
+      <DashboardMetrics />
 
       <AgrixiAssistant />
     </div>
