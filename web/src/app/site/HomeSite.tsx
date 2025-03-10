1528825d02130d2b3524a20aa5cc258057a6198e
vynnydev <vynnyprudencio@gmail.com>
2025-03-10 14:05:56 -0300

    feat: added removed site components

--- web/src/app/site/HomeSite.tsx
+++ web/src/app/site/HomeSite.tsx
@@ -1,28 +1,9 @@
 import React from 'react'
-import { HeroSection } from './components/HeroSection/HeroSection'
-import { AboutSection } from './components/AboutSection/AboutSection'
-import { ControlAgricultureSection } from './components/ControlAgricultureSection/ControlAgricultureSection'
-import { ServicesSection } from './components/ServicesSection/ServicesSection'
-import { WhyChooseUsSections } from './components/WhyChooseUsSections/WhyChooseUsSections'
-import { FrequentlyAskedQuestions } from './components/FrequentlyAskedQuestions/FrequentlyAskedQuestions'
-import { Footer } from './components/Footer/Footer'
-import { JoinUsSection } from './components/JoinUsSection/JoinUsSection'
-import MediMindAIAssistantSection from './components/MediMindAIAssistantSection/MediMindAIAssistantSection'
-import AccessibilityAssistantSection from './components/AccessibilityAssistantSection/AccessibilityAssistantSection'
 
 export const HomeSite = () => {
     return (
         <div>
-            <HeroSection />
-            <AboutSection />
-            <ControlAgricultureSection />
-            <ServicesSection />
-            <MediMindAIAssistantSection />
-            <WhyChooseUsSections />
-            <AccessibilityAssistantSection />
-            <FrequentlyAskedQuestions />
-            <JoinUsSection />
-            <Footer />
+            SITE H24 STUDIO
         </div>
     )
 }
